59d18a590c9ab26e2b461d56516e82a5bfc2d4c9
Andres Henderson <ahenderson.holberton@gmail.com>
2021-12-03 06:32:32 -0800

    addad

--- test/simple_shell.c
+++ test/simple_shell.c
@@ -8,30 +8,31 @@
  */
 int main(int ac, char **av, char **env)
 {
-	char *getcommand = NULL, **user_command = NULL;
+	char *line = NULL;
+	char **user_command = NULL;
 	int pathValue = 0, _exit = 0, n = 0;
 	(void)ac;
 
 	while (1)
 	{
-		getcommand = _getline_command();
-		if (getcommand)
+		line = _getline_command();
+		if (line)
 		{
 			pathValue++;
-			user_command = _get_token(getcommand);
+			user_command = _get_token(line);
 			if (!user_command)
 			{
-				free(getcommand);
+				free(line);
 				continue;
 			}
 			if ((!_strcmp(user_command[0], "exit")) && user_command[1] == NULL)
-				_exit_command(user_command, getcommand, _exit);
+				_exit_command(user_command, line, _exit);
 			if (!_strcmp(user_command[0], "env"))
 				_getenv(env);
 			else
 			{
 				n = _values_path(&user_command[0], env);
-				_exit = _fork_fun(user_command, av, env, getcommand, pathValue, n);
+				_exit = _fork_fun(user_command, av, env, line, pathValue, n);
 				if (n == 0)
 					free(user_command[0]);
 			}
@@ -43,7 +44,7 @@ int main(int ac, char **av, char **env)
 				write(STDOUT_FILENO, "\n", 1);
 			exit(_exit);
 		}
-		free(getcommand);
+		free(line);
 	}
 	return (_exit);
 }
